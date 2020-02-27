f209dc3c0adf870ab660d57cb76644f9b31c3c72
issacstorm <XFZgithub!@#123>
2020-02-27 00:50:36 -0500

    model

--- review_lda.py
+++ review_lda.py
@@ -4,6 +4,7 @@ import pdb
 import random
 import sqlite3 as lite
 from operator import itemgetter
+import pickle
 
 import spacy
 from gensim.corpora import Dictionary
@@ -29,57 +30,60 @@ class LDAReview():
             random.choice(self.sentences)))
         self.dictionary = dictionary = Dictionary(self.sentences)
         dictionary.filter_extremes(no_below=15, no_above=0.5, keep_n=100000)
-        bow_corpus = [dictionary.doc2bow(doc) for doc in self.sentences]
+        self.bow_corpus = bow_corpus = [
+            dictionary.doc2bow(doc) for doc in self.sentences]
         doc_len = len(self.bow_corpus)  # should equal to length of sentences
-        ids = random.shuffle(doc_len)
+        ids = list(range(doc_len))
+        random.shuffle(ids)
         self.shuffled2senid = {v: k for k, v in enumerate(ids)}
 
         test_ids = ids[:round(doc_len * test_ratio)]
         train_ids = ids[round(doc_len * test_ratio):]
 
-        self.bows = bow_corpus
         self.train_bows = [bow_corpus[i] for i in train_ids]
         self.test_bows = [bow_corpus[i] for i in test_ids]
 
     def cv(self):
         'cross validate the hyper-parameters. log_perplexity smaller the better'
         best_ppl = 1e6
-        best_para = {'topic': 10,
+        best_para = {'topic': 30,
                      'ps': 2,
-                     'alpha': 0.05,
-                     'beta': 0.05}
-        for topic in [10, 20, 30]:
-            for ps in [2, 4]:
-                for alpha in [0.05, 0.1, 0.5, 1, 5, 10]:
-                    for beta in [0.05, 0.1, 0.5, 1, 5, 10]:
-                        model = run(self.train_bows, self.dictionary,
-                                    num_topics=topic, passes=ps,
-                                    alpha=alpha, eta=beta)
-                        log_ppl = model.log_perplexity(self.test_bows)
-                        if log_ppl < best_ppl:
-                            best_ppl = log_ppl
-                            best_para['alpha'] = alpha
-                            best_para['beta'] = beta
-                            best_para['ps'] = ps
-                            best_para['topic'] = topic
+                     'alpha': 0.5,
+                     'beta': 5}
+        # for topic in [20, 30]:
+        #     for ps in [2, 4]:
+        #         for alpha in [0.05, 0.1, 0.5, 1, 5]:
+        #             for beta in [0.05, 0.1, 0.5, 1, 5]:
+        #                 model = run(self.train_bows, self.dictionary,
+        #                             num_topics=topic, passes=ps,
+        #                             alpha=alpha, eta=beta)
+        #                 log_ppl = model.log_perplexity(self.test_bows)
+        #                 if log_ppl < best_ppl:
+        #                     best_ppl = log_ppl
+        #                     best_para['alpha'] = alpha
+        #                     best_para['beta'] = beta
+        #                     best_para['ps'] = ps
+        #                     best_para['topic'] = topic
         alpha, beta, ps, topic = [best_para[i]
                                   for i in sorted(best_para, key=itemgetter(0))]
-        model = run(self.bows, self.dictionary,
+        print(best_para)
+        model = run(self.bow_corpus, self.dictionary,
                     num_topics=topic, passes=ps,
                     alpha=alpha, eta=beta)
         total_topics = {k: v for k, v in model.print_topics(-1)}
+        # pdb.set_trace()
         topic_weight = {self.senid2docid[self.shuffled2senid[i]]: model[j]
-                        for i, j in enumerate(self.bows)}
+                        for i, j in enumerate(self.bow_corpus)}
         # save the topics, weights for docs, and document id to the orginal id
-        with open('js_topics.json', 'w') as f:
-            json.dump(total_topics, f)
-        with open('js_weight.json', 'w') as f:
-            json.dump(topic_weight, f)
-        with open('js_docid2oldid', 'w') as f:
-            json.dump(self.docid2oldid, f)
+        with open('js_topics.json', 'wb') as f:
+            pickle.dump(total_topics, f)
+        with open('js_weight.json', 'wb') as f:
+            pickle.dump(topic_weight, f)
+        with open('js_docid2oldid', 'wb') as f:
+            pickle.dump(self.docid2oldid, f)
 
 
-def run(self, bow_corpus, dictionary,
+def run(bow_corpus, dictionary,
         num_topics=10, passes=2, n_workers=20, **kwargs):
     # best_ppl = 1e8
     model = LdaMulticore(bow_corpus,
@@ -115,9 +119,8 @@ def preprocess(id_reviews):
         docid2review[i] = reviews[i]
         sentences += sens
         for sen in sens:
-            if len(sen) > 0:
-                senid2docid[sen_id] = i
-                sen_id += 1
+            senid2docid[sen_id] = i
+            sen_id += 1
     return sentences, docid2oldid, docid2review, senid2docid
 
 
@@ -130,8 +133,8 @@ def loaddata():
 
 def main():
     id_reviews = loaddata()
-    lda = LDAReview(id_reviews)
-    lda.run()
+    lda = LDAReview(id_reviews, 0.3)
+    lda.cv()
     # pdb.set_trace()
     pass
 
